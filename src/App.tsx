bf99ad22fe6f92d129ad8f96ace6297b4cb23147
Manvi <131688388+manvi-ss@users.noreply.github.com>
2024-04-09 13:13:34 +0200

    fetched data from todos api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import React from "react";
+import { useState, useEffect } from "react";
 import { Typography } from "@mui/material";
 
 const App = () => {
-  return <Typography>Hello world</Typography>;
-};
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => {
+        setTodos(data);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <h2>{todo.title}</h2>
+        </div>
+      ))}
+    </div>
+  );
+};
 export default App;
